a39771f081123c224ab90a2d5027a7b82248bb60
Arber Mara <toxyverit20@gmail.com>
2023-01-30 09:01:37 +0100

    new item

--- src/main/java/net/toxyrock/eclypsmod/EclypsMod.java
+++ src/main/java/net/toxyrock/eclypsmod/EclypsMod.java
@@ -4,6 +4,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -12,6 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.toxyrock.eclypsmod.item.ModItems;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -26,8 +28,11 @@ public class EclypsMod {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public EclypsMod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/net/toxyrock/eclypsmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.toxyrock.eclypsmod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.toxyrock.eclypsmod.EclypsMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, EclypsMod.MOD_ID);
+
+        public static final RegistryObject<Item> ERINITE = ITEMS.register("erinite",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+
+        public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
